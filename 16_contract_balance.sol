6012b303c44f057e4d08fe5ad93ea8bb714960ae
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2023-04-06 18:01:16 +0530

    Add files via upload

---
+++ 16_contract_balance.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.5.0 <0.9.0;
+
+contract pay
+{
+    address payable user = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+    function payEther() public payable  //payable means eshpe transfer karna hai
+    {
+
+    }
+
+    function getBalance() public view returns(uint)
+    {
+        return address(this).balance;
+    }
+
+    function sendEtherAccount() public // shshe transfer karna hai
+    {
+        user.transfer(1 ether);
+    }
+}
